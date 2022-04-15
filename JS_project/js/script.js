834e19c4169022440f8e7ef7fc7ff3cb02689978
Denis Fisenko <monahSpb79@yandex.ru>
2022-04-15 17:47:37 +0300

    add server for forms

--- JS_project/js/script.js
+++ JS_project/js/script.js
@@ -202,4 +202,51 @@ window.addEventListener("DOMContentLoaded", () => {
     15,
     ".menu .container"
   ).render();
+
+  // Forms post server
+
+  const forms = document.querySelectorAll('form'),
+        message = {
+          loading: 'Загрузка',
+          success: 'Спасибо! Скоро мы с вами свяжемся.',
+          failure: 'Что - то пошло не так...'
+        };
+
+  forms.forEach(form => postData(form));
+  
+  function postData(form) {
+    form.addEventListener('submit', event => {
+      event.preventDefault();
+
+      const statusMessage = document.createElement('div');
+      statusMessage.classList.add('status');
+      statusMessage.textContent = message.loading;
+      form.append(statusMessage);
+
+      const request = new XMLHttpRequest();
+      request.open('POST', './server.php');
+
+      request.setRequestHeader('Content-type', 'application/json');
+      const formData = new FormData(form);
+
+      const object = {};
+      formData.forEach(function(value, key) {
+        object[key] = value;
+      });
+
+      const json = JSON.stringify(object);
+      request.send(json);
+
+      request.addEventListener('load', () => {
+        if (request.status === 200) {
+          console.log(request.response);
+          statusMessage.textContent = message.success;
+          form.reset();
+          setTimeout(() => statusMessage.remove(), 3000);
+        } else {
+          statusMessage.textContent = message.failure;
+        }
+      });
+    });
+  }
 });
